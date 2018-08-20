de71ae172c736fa51c7cbc34f30146244323aeb6
Damir Tulepov <damir@tulepov.com>
2018-08-20 05:29:28 +0300

    Rename variable in net_com_reference  example

--- tests/examples/example_binary/ShellComExample.cs
+++ tests/examples/example_binary/ShellComExample.cs
@@ -8,8 +8,8 @@ namespace ShellComExample
         [STAThread]
         static void Main(string[] args)
         {
-            var test = (Shell)Activator.CreateInstance(Type.GetTypeFromCLSID(new Guid("13709620-C279-11CE-A49E-444553540000")));
-            test.Open("shell:::{26EE0668-A00A-44D7-9371-BEB064C98683}");
+            var shell = (Shell)Activator.CreateInstance(Type.GetTypeFromCLSID(new Guid("13709620-C279-11CE-A49E-444553540000")));
+            shell.Open("shell:::{26EE0668-A00A-44D7-9371-BEB064C98683}");
         }
     }
 }
